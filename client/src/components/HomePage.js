097d433be99133f7f596675e6a0c573e5b4d667c
David Mostoller <dmostoller@gmail.com>
2024-08-12 18:54:51 -0400

    updated about

--- client/src/components/HomePage.js
+++ client/src/components/HomePage.js
@@ -15,12 +15,10 @@ return (
         <>
             <div className="ui two column stackable grid">
                 <div className="column">
-                    <div className="ui left aligned text basic very padded segment">
+                    <div className="ui left aligned text basic very padded segment" style={{marginTop: "25px"}}>
                         <h1 style={{fontSize: "3em"}}>I'm Yasmin,</h1>
-                            <h2>I believe in the power of creativity, imagination, and self-expression. 
-                                Art can evoke memories, transport us to new realms of thought and feeling, and offer fresh perspectives.</h2>
-                            <h2>Through my paintings, I seek to capture the moments that resonate with me, 
-                                allowing imagination and emotion to shape my vibrant abstract creations.</h2>
+                            <h1>I believe in the power of painting to evoke memories and transport us to new realms of thought and feeling.  
+                                Imagination and emotion shape my vibrant approach to abstract painting.</h1>
                     </div>
                     <div className="ui center aligned grid" style={{marginBottom: "25px"}}>
                         <Link to='/paintings' className="ui massive basic circular teal button"> Explore My Paintings</Link>
@@ -32,11 +30,13 @@ return (
                     <div className="ui huge rounded image">
                         <img src={headerImg} alt="header" />
                     </div>
+                    <h3 style={{textAlign: "center", marginTop:"25px", marginBottom: "0px"}}>
+                "To draw, you must close your eyes and sing." <i> -Pablo Picasso</i>
+            </h3>
                 </div>
                 </div>
             </div>
-            <h3 style={{textAlign: "center", marginTop:"75px", marginBottom: "0px"}}>
-                "To draw, you must close your eyes and sing." <i> -Pablo Picasso</i></h3>
+
 
                 {/* <div className="ui center aligned text container" style={{marginTop: "50px"}}>
                 <Link to='/contact' className="ui massive circular right labeled basic icon teal button"><i className="mail icon"></i>Get In Touch</Link>
