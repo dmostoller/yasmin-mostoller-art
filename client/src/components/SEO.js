069eda21d1ee3a1fd0d9bfe8a1ab38d914e4de5f
David Mostoller <dmostoller@gmail.com>
2024-08-13 23:34:18 -0400

    helmet provider

--- client/src/components/SEO.js
+++ client/src/components/SEO.js
@@ -15,10 +15,9 @@ export default function SEO({title, description, name, type, image, url}) {
             <meta property="og:description" content={description} />
             <meta property="og:image" content={image} />
             <meta property="og:url" content={url} />
-
             { /* End Facebook tags */ }
             { /* Twitter tags */ }
-            <meta name="twitter:creator" content={name} />}
+            <meta name="twitter:creator" content={name} />
             <meta name="twitter:card" content={type} />
             <meta name="twitter:title" content={title} />
             <meta name="twitter:description" content={description} />
